8a07f2cf8f7decf607a907cd952347a0ab553c34
Matti Salokangas <matti.salokangas@gmail.com>
2021-09-17 08:00:11 -0700

    ✅  Add tests for hook

--- hooks/AudioPlayerHooks.js
+++ hooks/AudioPlayerHooks.js
@@ -1,34 +1,43 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
 
-const useAudioPlayer = () => {
+const useAudioPlayer = ({ audioPlayer, progressBar, animationRef }) => {
   // state
+  const [isReady, setIsReady] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [timeJump, setTimeJump] = useState(0);
 
-  // references
-  const audioPlayer = useRef();   // reference our audio component
-  const progressBar = useRef();   // reference our progress bar
-  const animationRef = useRef();  // reference the animation
+  // flag for when ready
+  useEffect(() => {
+    setIsReady(Boolean(audioPlayer.current && progressBar.current));
+  }, [audioPlayer.current, progressBar.current]);
 
   // handle time jumps
   useEffect(() => {
+    if (!isReady) return;
+
     timeTravel(timeJump);
     setIsPlaying(true);
     play();
-  }, [timeJump])
+  }, [isReady, timeJump]);
 
   // grabs the loaded metadata
   useEffect(() => {
-    const seconds = Math.floor(audioPlayer.current.duration);
-    setDuration(seconds);
-    progressBar.current.max = seconds;
-  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState])
+    if (!isReady) return;
 
+    const seconds = Math.floor(audioPlayer.current.duration);
+    // setDuration(seconds); // Do we want to expose this?
+    progressBar.current.max = seconds;
+  }, [
+    isReady,
+    audioPlayer?.current?.loadedmetadata,
+    audioPlayer?.current?.readyState,
+  ]);
 
   // when you get to the end
   useEffect(() => {
+    // TODO: Add test for making sure this happens
     if (Number(duration) > 1 && Number(currentTime) === Number(duration)) {
       togglePlayPause();
       timeTravel(0);
@@ -41,12 +50,12 @@ const useAudioPlayer = () => {
     const seconds = Math.floor(secs % 60);
     const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
     return `${returnedMinutes}:${returnedSeconds}`;
-  }
+  };
 
   const play = () => {
     audioPlayer.current.play();
-    animationRef.current = requestAnimationFrame(whilePlaying)
-  }
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  };
 
   const togglePlayPause = () => {
     const prevValue = isPlaying;
@@ -57,54 +66,53 @@ const useAudioPlayer = () => {
       audioPlayer.current.pause();
       cancelAnimationFrame(animationRef.current);
     }
-  }
+  };
 
   const whilePlaying = () => {
     progressBar.current.value = audioPlayer?.current.currentTime;
     changePlayerCurrentTime();
     animationRef.current = requestAnimationFrame(whilePlaying);
-  }
+  };
 
   const changeRange = () => {
     audioPlayer.current.currentTime = progressBar.current.value;
     changePlayerCurrentTime();
-  }
+  };
 
   const changePlayerCurrentTime = () => {
-    progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`)
+    progressBar.current.style.setProperty(
+      "--seek-before-width",
+      `${(progressBar.current.value / duration) * 100}%`
+    );
     setCurrentTime(progressBar.current.value);
-  }
+  };
 
   const backThirty = () => {
     timeTravel(Number(progressBar.current.value) - 30);
-  }
+  };
 
   const forwardThirty = () => {
     timeTravel(Number(progressBar.current.value) + 30);
-  }
+  };
 
   const timeTravel = (newTime) => {
     progressBar.current.value = newTime;
     changeRange();
-  }
+  };
 
   return {
     isPlaying,
     duration,
     currentTime,
-    audioPlayer,
-    progressBar,
     calculateTime,
     togglePlayPause,
     changeRange,
     backThirty,
     forwardThirty,
     timeTravel,
-    setDuration,
-    setIsPlaying,
     setTimeJump,
-    play
-  }
-}
+    play,
+  };
+};
 
-export { useAudioPlayer }
+export { useAudioPlayer };
--- hooks/AudioPlayerHooks.test.js
+++ hooks/AudioPlayerHooks.test.js
@@ -1,22 +1,164 @@
-import { renderHook } from '@testing-library/react-hooks';
+import React from "react";
+import { act, renderHook } from "@testing-library/react-hooks";
 import { useAudioPlayer } from "./AudioPlayerHooks";
 
-describe('useAudioPlayer', () => {
-  test('renders the hook', () => {
-    const { result } = renderHook(() => useAudioPlayer());
+// 1) Meta data loads correctly:
+// - comes from js api
 
-    console.log(">>> Hook Result", result);
+// 2) Play / Pause
 
-    expect(result.current).toBe("???");
+// 3) Forward / Backward
+
+// 4) Arrange Slider / Jump to time
+
+// 5) Range slider progresses as audio progresses
+
+// 6) Pass in time and jump to it
+
+describe("useAudioPlayer", () => {
+  beforeEach(() => {
+    jest.spyOn(window, "requestAnimationFrame");
   });
 
+  afterEach(() => {
+    window.requestAnimationFrame.mockRestore();
+  });
 
-  test.todo('sets the total length of clip');
-  test.todo('handles time jumps');
+  const defaultProps = {
+    audioPlayer: {
+      current: {
+        currentTime: 0,
+        duration: 100,
+        pause: jest.fn(),
+        play: jest.fn(),
+      },
+    },
+    progressBar: {
+      current: {
+        max: 100,
+        style: {
+          setProperty: jest.fn(),
+        },
+        value: 0,
+      },
+    },
+    animationRef: {
+      current: {},
+    },
+  };
 
-  describe('when at the end', () => {
-    test.todo('jumps back to beginning');
+  const render = (overrides) =>
+    renderHook((props) => useAudioPlayer(props), {
+      initialProps: {
+        ...defaultProps,
+        ...overrides,
+      },
+    });
+
+  test("Meta data loads correctly", async () => {
+    const { result, waitFor } = render();
+
+    waitFor(() => {
+      expect(result.current.currentTime).toBe(0);
+      expect(result.current.duration).toBe(100);
+    });
+  });
+
+  test("play", () => {
+    const { result } = render();
+
+    result.current.play();
+
+    expect(defaultProps.audioPlayer.current.play).toHaveBeenCalled();
+    expect(window.requestAnimationFrame).toHaveBeenCalled();
+  });
+
+  // TODO: Test pause and cancelAnimationFrame
+  test("togglePlayPause", () => {
+    const { result, waitFor } = render();
+
+    expect(result.current.isPlaying).toBe(false);
+
+    result.current.togglePlayPause();
+
+    waitFor(() => {
+      expect(result.current.isPlaying).toBe(true);
+    });
+  });
+
+  test("timeTravel", () => {
+    const { result, waitFor } = render();
+
+    waitFor(() => {
+      result.current.timeTravel(50);
+
+      expect(defaultProps.progressBar.current.value).toBe(50);
+      expect(defaultProps.audioPlayer.current.currentTime).toBe(50);
+      expect(
+        defaultProps.progressBar.current.style.setProperty
+      ).toHaveBeenCalled();
+
+      expect(result.current.currentTime).toBe(50);
+    });
+  });
+
+  test("changeRange", () => {
+    const { result } = render();
+
+    defaultProps.progressBar.current.value = 1000;
+
+    result.current.changeRange();
+
+    expect(defaultProps.audioPlayer.current.currentTime).toBe(1000);
+    expect(result.current.currentTime).toBe(1000);
+    expect(
+      defaultProps.progressBar.current.style.setProperty
+    ).toHaveBeenCalled();
+  });
+
+  // TODO: Fix up this test
+  // test.only("setTimeJump", () => {
+  //   const { result, waitFor } = render();
+
+  //   result.current.setTimeJump(10);
+
+  //   waitFor(() => {
+  //     expect(defaultProps.progressBar.current.value).toBe(50);
+  //     expect(result.current.currentTime).toBe(10);
+  //   });
+  // });
+
+  // TODO: Add describe and test more branching to improve coverage
+  describe("calculateTime", () => {
+    test.each([
+      [120, "02:00"],
+      [60 * 20, "20:00"],
+      [60, "01:00"],
+      [30, "00:30"],
+    ])("when provided %is", (input, expected) => {
+      const { result } = render();
+
+      expect(result.current.calculateTime(input)).toBe(expected);
+    });
+  });
+
+  test("forwardThirty", () => {
+    const { result, waitFor } = render();
+
+    result.current.forwardThirty();
+
+    waitFor(() => {
+      expect(result.current.currentTime).toBe(30);
+    });
+  });
+
+  test("backThirty", () => {
+    const { result, waitFor } = render();
+
+    result.current.backThirty();
+
+    waitFor(() => {
+      expect(result.current.currentTime).toBe(0);
+    });
   });
 });
-
-
